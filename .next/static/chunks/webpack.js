3d0baafb9ce90378eeb160fc5c3b95034143942f
Akshay Benny <976Ab482a*123#>
2022-04-13 21:40:52 +0530

    bug fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ba37cbd9d6291402"; }
+/******/ 		__webpack_require__.h = function() { return "4d156922073fd2cc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/events/subs.js
+++ pages/events/subs.js
@@ -1,39 +1,46 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
-const Subs = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
+// const Subs = () => {
+//   const [data, setData] = useState([]);
+//   const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const dataFetcher = async () => {
-      setLoading(true);
-      try {
-        await fetch('/api/subs')
-          .then((res) => res.json())
-          .then((data) => setData(data.myData));
-       
-      } catch (error) {
-        console.log('Error fetching data>>>>', error);
-      }
-    };
-    dataFetcher();
-    setLoading(false);
-  });
-  return (
-    <div>
-      {loading && <p>Loading...</p>}
-      <ul className='space-y-2 px-12'>
-        {data.map((item, index) => {
-          return (
-            <li key={index} className='border-b-2 border rounded '>
-              <p className='font-bold'>{item.name}</p>
-              <p className='font-light'>{item.email}</p>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
-  );
-};
+//   useEffect(async () => {
+//     try {
+//       await fetch('/api/subs')
+//         .then((res) => res.json())
+//         .then((data) => setData(data.myData));
+//     } catch (error) {
+//       console.log('Error fetching data>>>>', error);
+//     }
+//   });
+//   console.log(loading);
 
-export default Subs;
+//   if (!data) {
+//     return <p>No data found</p>;
+//   }
+
+//   if (data.length === 0) {
+//     return <div>Loading...</div>;
+//   }
+
+//   return (
+//     <div>
+//       <ul className='space-y-2 px-12'>
+//         {loading ? (
+//           <p>Loading</p>
+//         ) : (
+//           data.map((item, index) => {
+//             return (
+//               <li key={index} className='border-b-2 border rounded '>
+//                 <p className='font-bold'>{item.name}</p>
+//                 <p className='font-light'>{item.email}</p>
+//               </li>
+//             );
+//           })
+//         )}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default Subs;
